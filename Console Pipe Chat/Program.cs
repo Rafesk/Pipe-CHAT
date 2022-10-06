ed76475a3e80782a3852c098caa000b7b5c068a5
Ksenia Balalaeva <balalaeva.ks@mail.ru>
2022-10-06 15:58:39 +0300

    06.10Para

---
+++ Console Pipe Chat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Console_Pipe_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NamedPipeServerStream pipe = new NamedPipeServerStream("myPipe", PipeDirection.InOut,2);
+            pipe.WaitForConnection();
+            Console.WriteLine("Пользователь подключился!");
+            StreamReader rd = new StreamReader(pipe);
+            StreamWriter wr = new StreamWriter(pipe);
+            wr.AutoFlush = true;
+            string str = rd.ReadLine();
+            Console.WriteLine("Read: " + str);
+            rd.Close();
+            wr.Close();
+        }
+    }
+}
--- Pipe Chat/Form1.cs
+++ Pipe Chat/Form1.cs
@@ -1,10 +1,29 @@
+using System.IO;
+using System.IO.Pipes;
 namespace Pipe_Chat
 {
     public partial class Form1 : Form
     {
+        NamedPipeClientStream pipe;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pipe = new NamedPipeClientStream("myPipe");
+            pipe.Connect();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader rd = new StreamReader(pipe);
+            StreamWriter wr = new StreamWriter(pipe);
+            wr.WriteLine(textBox1.Text);
+            textBox1.Text = "";
+            rd.Close();
+            wr.Close();
         }
     }
 }
